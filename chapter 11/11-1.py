ebf717e06f525251ce477c6b05cb5ca4c27e0fcd
Hyunjung Oh <jenny7120@naver.com>
2021-02-18 15:04:34 +0900

    02-18

---
+++ chapter 11/11-1.py
@@ -0,0 +1,13 @@
+# def dictionary(f):
+#     d = dict()
+#     count = 0
+#     for line in f:
+#         d[count] = line
+#         count += 1
+#         print(d[count])
+    
+
+
+# fin = open('test.txt')
+# print(dictionary(fin))
+print("hello world")
